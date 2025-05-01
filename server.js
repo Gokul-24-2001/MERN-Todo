8fd4eeefa5eba082b785200113440bd6f9cb0e1b
Gokul-24-2001 <gokulashok1002@gmail.com>
2025-05-01 16:12:04 +0530

    upadate,delete is done with db connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //using express
 const express=require("express");
-const mongose=require('mongoose')
+const mongoose=require('mongoose')
 // create a instance of express..object
 const app=express();
 app.use(express.json())
@@ -15,7 +15,10 @@ mongoose.connect('mongodb://localhost:27017/mern-app')
 
 // creating schema
 const todoschema=new mongoose.Schema({
-    tittle:String,
+    tittle:{
+        required:true,
+        type:String
+    },
     description:String
 })
 //creating model
@@ -36,14 +39,52 @@ try{
     res.status(201).json(newTodo)
 }
 catch(error){
-    console.log(error)
+    console.log(error);
+    res.status(500).json({message:error.message});
 }
 }
 )
 // get all items
-app.get('/todos',(req,res)=>{
-    res.json(storeTodos);
+app.get('/todos',async (req,res)=>{
+    try{
+      const todos= await todoModel.find();
+      res.json(todos);
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message:error.message});
+    }
 })
+//update the todos
+app.put('/todos/:id',async (req,res)=>{
+    try{
+        const {tittle,description} = req.body;
+    const id=req.params.id;
+       const updatedtodo=await todoModel.findByIdAndUpdate(id,{tittle,description},{new:id});
+       // if given 'id' is not find in database
+       if(!updatedtodo){
+        return res.status(404).json({message:"Todo is not found"})
+       }
+       res.status(200).json(updatedtodo)
+
+    }
+    catch(error){
+        res.status(500).json({message:error.message});
+    }
+})
+// delete item from database
+app.delete('/todos/:id', async(req,res)=>{
+try{
+    const id=req.params.id;
+ const deleteitem=await todoModel.findByIdAndDelete(id);
+ res.status(204).end()
+}
+catch(error){
+    console.log(error)
+    res.status(500).json({message:error.message});
+}
+})
+
 // start the serve
 const port=4000;
 app.listen(port,()=>console.log("server is listening to port"+" "+port))
